2d82b92cecda799d8b044d2dee491c0787843279
Chan <447880522@qq.com>
2016-07-14 17:19:56 +0800

    游戏大厅完善day 4!

--- gameclient-master/gameserver/index.js
+++ gameclient-master/gameserver/index.js
@@ -2,32 +2,85 @@
 var server = require('http').createServer();
 var io = require('socket.io')(server);
 var users = {};
-//socket就是客户端与服务器的通道
-io.on('connection', function(socket){
-	socket.join("public");
-  console.log("一个用户连接到服务器");
-  
-  socket.on('user.online',function(user){
-  	user.room= 'public';
-		users[user.id]=user;//保存用户信息
-		//广播也用户信息
-		io.sockets.emit('user.online',getUser());
-  });
-socket.on('chat.send',function(chat){
-	var user = users[socket.id.replace("/#",'')];
-	socket.to(user.room).emit("chat.newchat",chat);
-})
+var rooms = {};
 
-  socket.on('disconnect', function(){
-     delete users[socket.id.replace("/#",'')];
-     io.sockets.emit('user.online',getUser());
-  });
+//socket就是客户端与服务器的通道
+io.on('connection', function(socket) {
+	//	socket.join("public");
+	console.log("一个用户连接到服务器");
+
+	socket.on('user.online', function(user) {
+		user.room = 'public';
+		socket.join(user.room);
+		users[user.id] = user; //保存用户信息
+		//广播也用户信息
+		io.sockets.emit('user.online', getArr(users));
+		io.sockets.in("public").emit("room.rooms", getArr(rooms));
+	});
+
+	socket.on('room.create', function(roomname) {
+		if(rooms[roomname]) {
+			socket.emit("rooms.existroom");
+			return;
+		}
+		//因为后端的socket.id 比前端多了一个／＃需要用正则判断去掉
+		var user = users[socket.id.replace("/#", '')];
+		rooms[roomname] = {
+			roomname: roomname,
+			player1: user,
+			player2: null
+		};
+		//离开当前房间，进入新的房间
+		socket.leave(user.room);
+		user.room = roomname;
+		socket.join(user.room);
+		user.status = 2;
+		//广播房间信息
+		io.sockets.in("public").emit("room.rooms", getArr(rooms));
+		//通知玩家
+		socket.emit("room.success", rooms[roomname]);
+		//更新状态新消息
+		io.sockets.emit('user.online', getArr(users));
+	});
+
+	socket.on('chat.send', function(chat) {
+		var user = users[socket.id.replace("/#", '')];
+		socket.to(user.room).emit("chat.newchat", chat);
+	})
+
+	socket.on('disconnect', function() {
+		delete users[socket.id.replace("/#", '')];
+		io.sockets.emit('user.online', getArr(users));
+	});
+	//join room
+	socket.on("room.join", function(roomname) {
+		if(rooms[roomname].player2 != null) {
+			socket.emit("room.full");
+			return;
+		}
+		var user = users[socket.id.replace("/#", '')];
+		socket.leave(user.room);
+		user.room = roomname;
+		socket.join(user.room);
+		user.status = 2;
+		rooms[roomname].player2 = user;
+		//广播房间信息
+		io.sockets.in("public").emit("room.rooms", getArr(rooms));
+		//通知玩家
+		socket.emit("room.joinOK", rooms[roomname]);
+		 //通知房主
+		 socket.in(roomname).emit("room.success",rooms[roomname]);
+		//更新状态新消息
+		io.sockets.emit('user.online', getArr(users));
+	})
+	
 });
-function getUser(){
+//object转为数组
+function getArr(arrname) {
 	var arr = [];
-	for(var key in users)//循环users
+	for(var key in arrname) //循环users
 	{
-		arr.push(users[key]);//把users 的值传值
+		arr.push(arrname[key]); //把users 的值传值
 	}
 	return arr;
 }
